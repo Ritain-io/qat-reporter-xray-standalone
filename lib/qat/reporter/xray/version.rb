2ae934b50359b0fd37303dad8cb2ed0b414478a5
Leal <joao.leal@ritain.io>
2023-03-09 15:11:11 +0000

    #Return publisher on configuration

--- lib/qat/reporter/xray/version.rb
+++ lib/qat/reporter/xray/version.rb
@@ -5,7 +5,7 @@ module QAT
     # Namespace for QAT Reporter's Xray integrations
     class Xray
       # Represents QAT Reporter's Xray integrations' version
-      VERSION = '1.0.6'
+      VERSION = '1.0.7'
     end
   end
 end
